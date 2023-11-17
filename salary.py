29242c773540301ec39eb76b9db5a90ea8c31185
Aidan Cooney <acooney613@gmail.com>
2023-11-17 11:10:19 -0700

    completed

--- salary.py
+++ salary.py
@@ -3,8 +3,6 @@ from bs4 import BeautifulSoup
 import requests
 import datetime
 import re
-import numpy as np
-import time
 
 POP2010 = 'Intercensal Estimates of the Resident Population for Incorporated Places and Minor Civil Divisions: April 1, 2000 to July 1, 2010'
 
@@ -71,6 +69,7 @@ STATES = {
 class combine():
     def __init__(self, salary, stadium, population, attendance):
         self.stad_att(stadium, attendance)
+        print(self.data[self.data['year'] == '2003'])
         self.pop_stad_att(population)
         # deletes the royals ones why????
         self.sal_pop_stad_att(salary)
@@ -78,7 +77,7 @@ class combine():
     def sal_pop_stad_att(self, salary):
         data = self.data
         data = pd.merge(self.data, salary, on = ['team', 'year'])
-        data.to_csv('test.csv')
+        data.to_csv('mlb_attendance.csv')
     
     def pop_stad_att(self, population):
         data = self.data
@@ -255,7 +254,7 @@ class stadiums():
                 closed = closed.group(1)
                 closed = re.search(r',\s*(.*?)(?:,|\s*\(|$)', closed).group(1)
             
-            if int(closed) > 2003:
+            if int(closed) >= 2003:
                 row = {'team' : team, 'location' : location, 'stadium' : name, 'capacity' : capacity, 'opened' : opened, 'closed' : closed}
                 self.data = pd.concat([self.data, pd.DataFrame(data = row, index = [len(self.data) + 1])], ignore_index = True)
             tmp_r.close()
@@ -300,10 +299,6 @@ class stadiums():
         data['state'] = data['state'].map(STATES)
         data['location'] = data['city'] + ', ' + data['state']
         self.data = data[['team', 'location', 'stadium', 'capacity', 'opened', 'closed']].dropna().reset_index(drop = True)
-
-        # need to merge on both the team and the year, to get the right stadium and location
-        # need to add a year column to the attendance dataset or something
-
 
 class attendance():
     def __init__(self, base_url):
@@ -357,90 +352,6 @@ class attendance():
                 self.collect(self.base_url + extend, year)
                 year -= 1
 
-
-
-class salary():
-    def __init__(self, bat, pitch):
-        # this website has a too many requests error
-        self.url_bat = bat
-        self.url_pitch = pitch
-    
-    def get_data(self):
-        self.data_bat = pd.DataFrame(columns = ['team', 'bat_salary', 'year'])
-        self.data_pitch = pd.DataFrame(columns = ['team', 'pitch_salary', 'year'])
-        self.batter_data(self.url_bat)
-        self.pitcher_data(self.url_pitch)
-        self.clean()
-        #self.pitcher_data(self.url_pitch)
-        #self.combine()
-        # only want the overall salary
-        # need to get for different years !!!!
-        return self.data
-
-
-    def batter_data(self, url):
-        time.sleep(3)
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
-    
-        year = soup.find('h1').text
-        year = re.search(r'(\d+)', year).group(1)
-
-        print(year)
-        table = soup.find('table', class_ = 'stats_table')
-        tbody = table.find('tbody')
-        teams = tbody.find_all('tr')
-
-        for team in teams:
-            row = {'team' : team.find('a').text, 'bat_salary' : team.find('td', {'data-stat' : 'Salary'}).text, 'year' : year}
-            self.data_bat = pd.concat([self.data_bat, pd.DataFrame(data = row, index = [len(self.data_bat) + 1])], ignore_index = True)
-
-        #self.data['bat_salary'] = self.data['bat_salary'].str.replace(',', '')
-        #self.data['bat_salary'] = self.data['bat_salary'].str.extract(r'(\d+)').astype('int')
-    
-        if int(year) > 2003:
-            base_url = 'https://www.baseball-reference.com'
-            prev_url = soup.find('a', href = True, class_ = 'button2 prev')
-            self.batter_data(base_url + prev_url['href'])
-
-        r.close()
-
-    def pitcher_data(self, url):
-        time.sleep(3)
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text ,'html.parser')
-
-        year = soup.find('h1').text
-        year = re.search(r'(\d+)', year).group(1)
-        table = soup.find('table', class_ = 'stats_table')
-        tbody = table.find('tbody')
-        teams = tbody.find_all('tr')
-
-        for team in teams:
-            row = {'team' : team.find('a').text, 'pitch_salary' : team.find('td', {'data-stat' : 'Salary'}).text, 'year' : year}
-            self.data_pitch = pd.concat([self.data_pitch, pd.DataFrame(data = row, index = [len(self.data_pitch) + 1])], ignore_index = True)
-        
-        #data['pitch_salary'] = data['pitch_salary'].str.replace(',', '')
-        #data['pitch_salary'] = data['pitch_salary'].str.extract(r'(\d+)').astype('int')
-        if int(year) > 2003:
-            base_url = 'https://www.baseball-reference.com'
-            prev_url = soup.find('a', href = True, class_ = 'button2 prev')
-            self.pitcher_data(base_url + prev_url['href'])
-        r.close()
-    
-    def clean(self):
-        self.data_bat['bat_salary'] = self.data_bat['bat_salary'].str.replace(',', '')
-        self.data_bat['bat_salary'] = self.data_bat['bat_salary'].str.extract(r'(\d+)').astype('int')
-        self.data_pitch['pitch_salary'] = self.data_pitch['pitch_salary'].str.replace(',', '')
-        self.data_pitch['pitch_salary'] = self.data_pitch['pitch_salary'].str.extract(r'(\d+)').astype('int')
-
-    def combine(self):
-        bat = self.data_bat
-        pitch = self.data_pitch
-        data = pd.merge(bat, pitch, on = ['team', 'year'])
-        data['salary'] = data['bat_salary'] + data['pitch_salary'] 
-        self.data = data
-
 class payroll():
     def __init__(self, base_url):
         self.base_url = base_url
@@ -473,19 +384,11 @@ class payroll():
         if year > 2003:
             self.payroll(year - 1)
 
-#x = salary('https://www.baseball-reference.com/leagues/majors/2023-value-batting.shtml', 
- #        'https://www.baseball-reference.com/leagues/majors/2023-value-pitching.shtml')
-
-#df_salary = x.get_data()
-#df_salary.to_csv('salary.csv')
-#print(df_salary)
-
 x = payroll('https://www.thebaseballcube.com/page.asp?PT=payroll_year&ID=')
 df_payroll = x.get_data()
 
 y = attendance('https://www.espn.com/mlb/attendance')
 df_attendance = y.get_data()
-
 
 z = stadiums('https://www.ballparksofbaseball.com/american-league/', 'https://www.ballparksofbaseball.com/national-league/', 
             'https://www.ballparksofbaseball.com/past-ballparks/')
@@ -498,7 +401,3 @@ t = population('https://www.census.gov/data/tables/time-series/demo/popest/2020s
 df_pop = t.get_data()
 
 s = combine(df_payroll, df_stadium, df_pop, df_attendance)
-
-
-# padres, phillies 2003
-
