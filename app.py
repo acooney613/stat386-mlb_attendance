ff2f817af3ed615623a23ae38925b233d876f27b
Aidan Cooney <acooney613@gmail.com>
2023-11-26 16:40:55 -0700

    update

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ df = pd.read_csv('mlb_attendance.csv')
 df['proportion of capacity filled'] = df['average attendance'] / df['capacity']
 fig = px.scatter(df, x = 'payroll', y = 'proportion of capacity filled', color = 'team')
 
+st.title('Proportion of Stadium Capacity Filled')
 st.plotly_chart(fig)
