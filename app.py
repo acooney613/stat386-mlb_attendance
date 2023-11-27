8595fa161da11534a85bcc11796778c63812c8bf
Aidan Cooney <acooney613@gmail.com>
2023-11-26 21:49:37 -0700

    update

--- app.py
+++ app.py
@@ -7,13 +7,17 @@ import matplotlib.pyplot as plt
 st.title('MLB Attendance')
 
 data = pd.read_csv('mlb_attendance.csv')
-year = st.number_input('Enter Year', value = None, placeholder = 'enter year between 2003 and 2022', min_value = 2003, max_value = 2022)
+year = st.number_input('Enter Year', value = None, placeholder = 'enter year between 2003 and 2022', min_value = 2003, 
+                       max_value = 2022)
 
 data['proportion of capacity filled'] = data['average attendance'] / data['capacity']
-
 df = data[data['year'] == year]
-fig1 = px.scatter(df, x = 'payroll', y = 'proportion of capacity filled', color = 'team', trendline = 'ols', 
+if year != 2020:
+    fig1 = px.scatter(df, x = 'payroll', y = 'proportion of capacity filled', color = 'team', trendline = 'ols', 
                   trendline_scope = 'overall', trendline_color_override = 'orange')
+    
+else:
+    fig1 = px.scatter(df, x = 'payroll', y = 'proportion of capacity filled', color = 'team')
 
 st.plotly_chart(fig1)
 
