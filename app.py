d4e6618634dfae106efeaee6166862127a11cb68
Aidan Cooney <acooney613@gmail.com>
2023-11-26 19:54:13 -0700

    update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.express as px
 st.title('MLB Attendance')
 
 data = pd.read_csv('mlb_attendance.csv')
-year = st.number_input('Enter Year', value = 2022, placeholder = 'between 2003 and 2022', min_value = 2003, max_value = 2022)
+year = st.number_input('Enter Year', value = 2022, placeholder = 'enter year between 2003 and 2022', min_value = 2003, max_value = 2022)
 
 data['proportion of capacity filled'] = data['average attendance'] / data['capacity']
 
