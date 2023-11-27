6c978672c6e5289fe42dde5f3362553d06c17f9b
Aidan Cooney <acooney613@gmail.com>
2023-11-26 19:53:37 -0700

    update

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ import plotly.express as px
 st.title('MLB Attendance')
 
 data = pd.read_csv('mlb_attendance.csv')
-year = st.text_input('Enter Year', '2022')
+year = st.number_input('Enter Year', value = 2022, placeholder = 'between 2003 and 2022', min_value = 2003, max_value = 2022)
 
 data['proportion of capacity filled'] = data['average attendance'] / data['capacity']
 
-df = data[data['year'] == int(year)]
-print(df)
+df = data[data['year'] == year]
 fig = px.scatter(df, x = 'payroll', y = 'proportion of capacity filled', color = 'team')
 
 st.plotly_chart(fig)
