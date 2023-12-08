cff13b7125477e6ec113f66c4c19ad9fdab6d9a8
Aidan Cooney <acooney613@gmail.com>
2023-12-08 13:38:40 -0700

    updates

---
+++ population.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+POP2010 = 'Intercensal Estimates of the Resident Population for Incorporated Places and Minor Civil Divisions: April 1, 2000 to July 1, 2010'
+
+class population():
+    def __init__(self, url_1, url_2, url_3, url_4, url_5, location):
+        self.location = location
+        self.url_1 = url_1
+        self.url_2 = url_2
+        self.url_3 = url_3
+        self.url_4 = url_4
+        self.url_5 = url_5
+        self.canada = pd.DataFrame(columns = ['year', 'population', 'location'])
+
+
+    def get_data(self):
+        self.population_2022(self.url_1)
+        self.population_2019(self.url_2)
+        self.population_2009(self.url_3)
+        self.canadian_data(self.url_4, 'Toronto')
+        self.canadian_data(self.url_5, 'Montreal')
+        self.combine()
+        return self.data
+
+    def combine(self):
+        df = pd.merge(self.pop_2010, self.pop_2019, on = 'location')
+        df = pd.merge(df, self.pop_2023, on = 'location')
+        df = df.melt(id_vars = 'location', 
+                     value_vars = ['2003', '2004', '2005', '2006', '2007', '2008', '2009',
+                                    '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017',
+                                    '2018', '2019', '2020', '2021', '2022'],
+                     value_name = 'population',
+                     var_name = 'year')
+        
+        df = pd.concat([df, self.canada], ignore_index = True)
+        df['year'] = df['year'].astype('int')
+        df = df[df['year'] > 2002]
+        df = df[df['year'] < 2023].reset_index(drop = True)
+        self.data = df
+        df.to_csv('population.csv')
+
+    def canadian_data(self, url, city):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        table = soup.find('table', class_ = 'tp-table-body is-narrow w-full min-w-full table-auto border-separate border-spacing-0 border bg-white')
+        items = table.find_all('tr')
+        for item in items:
+            year = item.find('th')
+            pop = item.find('td')
+            if pop:
+                population = pop.text.strip()
+                population = population.replace(',', '')
+        
+                row = {'year' : year, 'population' : population, 'location' : f'{city}, Canada'}
+                self.canada = pd.concat([self.canada,pd.DataFrame(data = row, index = [len(self.canada) + 1])], ignore_index=True)
+
+
+    def population_2022(self, url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        us = soup.find('li', class_ = 'uscb-list-attachment')
+        link = us.find('a', href = True)
+
+        pop_2023 = pd.read_excel('https:' + link['href']).dropna()
+        pop_2023.columns = ['location', 'april 2020 base', '2020', '2021', '2022']
+        pop_2023['location'] = pop_2023['location'].str.replace(' city', '')
+        pop_2023['location'] = pop_2023['location'].str.replace(' City', '')
+        pop_2023['2021'] = pop_2023['2021'].astype('int')
+        pop_2023['2022'] = pop_2023['2022'].astype('int')
+        mask = pop_2023['location'].isin(self.location)
+        pop_2023 = pop_2023[mask]
+        pop_2023 = pop_2023[['location', '2020', '2021', '2022']].reset_index(drop = True)
+        self.pop_2023 = pop_2023
+
+    def population_2019(self, url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        us = soup.find('li', class_ = 'uscb-list-attachment')
+        link = us.find('a', href = True)
+
+        pop_2019 = pd.read_excel('https:' + link['href']).dropna().reset_index(drop = True)
+        pop_2019.columns = ['location', 'census', 'estimate base', '2010', '2011', '2012', '2013', '2014', '2015',
+                             '2016', '2017', '2018', '2019']
+        
+        pop_2019['location'] = pop_2019['location'].str.replace(' city', '')
+        pop_2019['location'] = pop_2019['location'].str.replace(' City', '')
+        pop_2019['2011'] = pop_2019['2011'].astype('int')
+        pop_2019['2012'] = pop_2019['2012'].astype('int')
+        pop_2019['2013'] = pop_2019['2013'].astype('int')
+        pop_2019['2014'] = pop_2019['2014'].astype('int')
+        pop_2019['2015'] = pop_2019['2015'].astype('int')
+        pop_2019['2016'] = pop_2019['2016'].astype('int')
+        pop_2019['2017'] = pop_2019['2017'].astype('int')
+        pop_2019['2018'] = pop_2019['2018'].astype('int')
+        pop_2019['2019'] = pop_2019['2019'].astype('int')
+
+        mask = pop_2019['location'].isin(self.location)
+        pop_2019 = pop_2019[mask]
+
+        pop_2019 = pop_2019[['location', '2010', '2011', '2012', '2013', '2014', '2015', '2016',
+                             '2017', '2018', '2019']].reset_index(drop = True)
+        
+        self.pop_2019 = pop_2019
+    
+    def population_2009(self, url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        us = soup.find('a', {'name' : POP2010}, href = True)
+        new_url = 'https:' + us['href']
+        df = pd.read_csv(new_url, encoding = 'ISO-8859-1')
+        df = df[['NAME', 'STNAME', 'POPESTIMATE2003', 'POPESTIMATE2004', 'POPESTIMATE2005',
+                            'POPESTIMATE2006', 'POPESTIMATE2007', 'POPESTIMATE2008', 'POPESTIMATE2009',
+                            'POPESTIMATE2010']]
+        df.columns = ['NAME', 'STNAME', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010']
+        df[['location']] = df['NAME'].str.extract(r'(.*?)(?:\s*city|\s*City)')
+        df = df.dropna()
+        df['location'] = df['location'] + ', ' + df['STNAME']
+        mask = df['location'].isin(self.location)
+        df = df[mask]
+        df = df[['location', '2003', '2004', '2005', '2006', '2007', '2008', '2009']]
+        
+        df = df.drop_duplicates('location', keep = 'first').reset_index(drop = True)
+        self.pop_2010 = df
+
+tmp = pd.read_csv('stadiums.csv')
+t = population('https://www.census.gov/data/tables/time-series/demo/popest/2020s-total-cities-and-towns.html',
+              'https://www.census.gov/data/tables/time-series/demo/popest/2010s-total-cities-and-towns.html',
+              'https://www.census.gov/data/datasets/time-series/demo/popest/intercensal-2000-2010-cities-and-towns.html',
+              #'https://www.macrotrends.net/cities/20402/toronto/population',
+              'https://worldpopulationreview.com/canadian-cities/toronto-population',
+              #'https://www.macrotrends.net/cities/20384/montreal/population',
+              'https://worldpopulationreview.com/canadian-cities/montreal-population',
+              tmp['location'])
+df_pop = t.get_data()
+
+print(df_pop)
---
+++ season.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+import datetime
+
+class seasons():
+    def __init__(self):
+        self.get_standings()
+        self.get_results()
+        self.merge()
+
+    def merge(self):
+        
+        season = pd.merge(self.standings, self.result, how = 'outer', on = ['team', 'year'])
+        season = season.fillna('Missed Postseason')
+
+        season['team'] = season['team'].str.replace('Los Angeles Angels of Anaheim', 'Los Angeles Angels')
+        season['team'] = season['team'].str.replace('Indians', 'Guardians')
+        season.to_csv('season.csv')
+
+
+    def get_standings(self):
+        dt = datetime.date.today()
+        year = dt.year - 1
+        win_loss = pd.DataFrame(columns = ['Tm', 'W', 'L', 'W-L%', 'year'])
+        while year > 2002:
+            url = f'https://www.baseball-reference.com/leagues/majors/{year}-standings.shtml'
+            dfs = pd.read_html(url)
+            east_al = dfs[0]
+            central_al = dfs[1]
+            west_al = dfs[2]
+            east_nl = dfs[3]
+            central_nl = dfs[4]
+            west_nl = dfs[5]
+            df = pd.concat([east_al, central_al, west_al, east_nl, central_nl, west_nl], ignore_index = True)
+            df['year'] = year
+            df = df[['Tm', 'W', 'L', 'W-L%', 'year']]
+            win_loss = pd.concat([win_loss, df], ignore_index = True)
+            year -= 1
+        win_loss.columns = ['team', 'wins', 'losses', 'w-l%', 'year']
+        self.standings = win_loss
+
+    def get_results(self):
+        url = 'https://www.baseball-reference.com/postseason/'
+        df = pd.read_html(url)[0]
+        df = df.dropna()
+        df.columns = ['Series', 'Games', 'Teams']
+        df[['year', 'series']] = df['Series'].str.extract(r'(\d+)(.*)')
+        df[['series']] = df['series'].str.extract(r'\s*(.*?)(?=\d|$)')
+        df['year'] = df['year'].astype('int')
+        df = df[df['year'] > 2002]
+        df = df[df['year'] < 2023]
+        df[['winner']] = df['Teams'].str.extract(r'(.*?)\**\s*\(')
+        df[['loser']] = df['Teams'].str.extract(r'vs.\s*(.*?)\**\s*\(')
+        df1 = df[['year', 'series', 'winner']]
+        df2 = df[['year', 'series', 'loser']]
+        df1['result'] = 'won'
+        df2['result'] = 'lost'
+        df1.columns = ['year', 'series', 'team', 'result']
+        df2.columns = ['year', 'series', 'team', 'result']
+
+        data = pd.concat([df1, df2], ignore_index=True)
+        order = ['World Series', 'ALCS', 'NLCS', 'ALDS', 'NLDS', 'ALWC', 'NLWC']
+        data['series'] = pd.Categorical(data['series'], categories = order)
+        data = data.sort_values(by = ['series', 'year'])
+        data = data.drop_duplicates(subset = ['team', 'year'], keep = 'first').reset_index(drop = True)
+        data['series'] = data['series'].astype('string')
+        self.result = data
+
+t = seasons()
+
+# toronto population data https://www.macrotrends.net/cities/20402/toronto/population https://www.macrotrends.net/cities/20384/montreal/population
