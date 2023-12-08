50c52f6edd22ab827c31a3696cca5523ca00d14c
Aidan Cooney <acooney613@gmail.com>
2023-12-08 12:33:11 -0700

    stadium data

---
+++ stadiums.py
@@ -0,0 +1,163 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import re
+
+STATES = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming',
+        'ON': 'Canada',
+        'PQ': 'Canada'
+    }
+
+class stadiums():
+    def __init__(self, url_1, url_2, url_3):
+        self.data = pd.DataFrame(columns = ['team', 'location', 'stadium', 'capacity', 'opened', 'closed'])
+        self.url_1 = url_1
+        self.url_2 = url_2
+        self.url_3 = url_3
+    
+    def get_data(self):
+        self.stadium_data(self.url_1)
+        self.stadium_data(self.url_2)
+        self.past_stadium(self.url_3)
+        self.clean()
+        return self.data
+
+    def past_stadium(self, url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        ballparks = soup.find_all('a', class_ = 'stadium-item', href = True)
+
+        for ballpark in ballparks:
+            name = ballpark.find('div', class_ = 'title').text
+            location = ballpark.find('div', class_ = 'city').text.strip()
+            tmp_r = requests.get(ballpark['href'])
+            tmp_soup = BeautifulSoup(tmp_r.text, 'html.parser')
+            info = tmp_soup.find_all('div', class_ = 'facts-col')
+            tmp = ''
+            for x in info:
+                tmp = tmp + x.find('p').text + '\n'
+            
+            team = re.search(r'(?:Tenant|Tenants):\s*(.*?)\n', tmp).group(1)
+            capacity = re.search(r'Capacity:\s*(.*?)\n', tmp).group(1)
+            opened = re.search(r'(?:Opened|Opening):\s*(.*?)\n', tmp).group(1)
+            closed = re.search(r'Closed:\s*(.*?)\n', tmp)
+            if name == 'RFK STADIUM':
+                opened = ', 2005'
+            if closed == None:
+                closed = self.data[self.data['location'] == location]['opened'].to_string()
+                closed = re.search(r',\s*(.*?)(?:,|\s*\(|$)', closed).group(1)
+                closed = int(closed) - 1
+                closed = f'{closed}'
+
+                
+            else:
+                closed = closed.group(1)
+                closed = re.search(r',\s*(.*?)(?:,|\s*\(|$)', closed).group(1)
+            
+            if int(closed) >= 2003:
+                row = {'team' : team, 'location' : location, 'stadium' : name, 'capacity' : capacity, 'opened' : opened, 'closed' : closed}
+                self.data = pd.concat([self.data, pd.DataFrame(data = row, index = [len(self.data) + 1])], ignore_index = True)
+            tmp_r.close()
+        r.close()
+
+
+        
+    def stadium_data(self, url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        ballparks = soup.find_all('a', class_ = 'stadium-item', href = True)
+
+        for ballpark in ballparks:
+            name = ballpark.find('div', class_ = 'title').text
+            location = ballpark.find('div', class_ = 'city').text.strip()
+            tmp_r = requests.get(ballpark['href'])
+            tmp_soup = BeautifulSoup(tmp_r.text, 'html.parser')
+            info = tmp_soup.find('div', class_ = 'facts-col')
+            info = info.find('p').text
+            team = re.search(r'-(?:Tenant|Tenants):\s*(.*?)\n', info).group(1)
+            capacity = re.search(r'-Capacity:\s*(.*?)\n', info).group(1)
+            opened = re.search(r'-(?:Opened|Opening):\s*(.*?)\n', info).group(1)
+
+            row = {'team' : team, 'location' : location, 'stadium' : name, 'capacity' : capacity, 'opened' : opened, 'closed' : '-'}
+            self.data = pd.concat([self.data, pd.DataFrame(data = row, index = [len(self.data) + 1])], ignore_index = True)
+            tmp_r.close()
+        r.close()
+    
+    def clean(self):
+        data = self.data
+        data[['team']] = data['team'].str.extract(r'\s*(.*?)(?:,|\(|$)')
+        data['team'] = data['team'].str.strip()
+        data['team'] = data['team'].str.replace('Florida', 'Miami')
+        data['team'] = data['team'].str.replace('Indians', 'Guardians')
+        data['location'] = data['location'].str.strip()
+        data['capacity'] = data['capacity'].str.replace(',', '')
+        data['capacity'] = data['capacity'].str.extract(r'(\d+)').astype('int')
+        data[['city']] = data['location'].str.extract(r'(.*?)(?:,)')
+        data['city'] = data['city'].replace(['Bronx', 'Queens', 'Flushing'], 'New York City')
+        data[['state']] = data['location'].str.extract(r',\s*(\w+)')
+        data[['opened']] = data['opened'].str.extract(r',\s*(.*?)(?:,|\s*\(|$)')
+        data[['city']] = data['city'].str.extract(r'(.*?)(?:\s*City|$)')
+        data['state'] = data['state'].map(STATES)
+        data['location'] = data['city'] + ', ' + data['state']
+        self.data = data[['team', 'location', 'stadium', 'capacity', 'opened', 'closed']].dropna().reset_index(drop = True)
+
+z = stadiums('https://www.ballparksofbaseball.com/american-league/', 'https://www.ballparksofbaseball.com/national-league/', 
+            'https://www.ballparksofbaseball.com/past-ballparks/')
+df_stadium = z.get_data()
