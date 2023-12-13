9c976f575d43cd402fd69e637939a9a591f5b896
Aidan Cooney <acooney613@gmail.com>
2023-12-13 15:51:11 -0700

    update

--- app.py
+++ app.py
@@ -6,17 +6,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 st.title('MLB Attendance Data Exploration')
+text = '<span style="color: red;">The MLB does not have attendance numbers for the year 2020 due to COVID-19</span>'
+st.markdown(text, unsafe_allow_html = True)
 
 data = pd.read_csv('mlb_attendance.csv')
+numbering = {'World Series' : 4, 'NLCS' : 3, 'ALCS' : 3, 'ALDS' : 2, 'NLDS' : 2, 'NLWC' : 1, 'ALWC' : 1, 'Missed Postseason' : 0}
+data['postseason'] = data['series'].map(numbering)
 data['proportion'] = data['average attendance'] / data['capacity']
 data['series_result'] = data['result'] + ' ' + data['series']
 data['series_result'] = data['series_result'].fillna('missed postseason')
 data['made postseason'] = np.where(data['series'] != 'Missed Postseason', 'Yes', 'No')
-numbering = {'World Series' : 4, 'NLCS' : 3, 'ALCS' : 3, 'ALDS' : 2, 'NLDS' : 2, 'NLWC' : 1, 'ALWC' : 1, 'Missed Postseason' : 0}
-data['postseason'] = data['series'].map(numbering)
 
 st.subheader('Attendance By Payroll')
 st.info('NOTE: The Montreal Expos moved to Washington D.C. in 2005 and became the Washington Nationals')
+
 year = st.number_input('Enter Year', value = None, placeholder = 'enter year between 2003 and 2022', min_value = 2003, 
                        max_value = 2022)
 
@@ -39,6 +42,7 @@ st.plotly_chart(fig1)
 options = data['team'].unique()
 options.sort()
 
+st.subheader('Boxplots Of Team Average Attendance')
 selected = st.multiselect('Select teams:', options = options, default = None)
 
 df2 = data[data['team'].isin(selected)]
@@ -49,6 +53,11 @@ if df2.empty:
 else:
     fig2.update_xaxes(range=[min(df2['average attendance']) - 5000, max(df2['average attendance']) + 5000])
 
+fig2.update_layout(
+    xaxis_title='Attendance',
+    yaxis_title='Teams Selected',
+    title='Attendence Plots For Teams Selected'
+)
 
 st.plotly_chart(fig2)
 
