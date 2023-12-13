b537fbfb786b961d15513d2a010f8ed1a7d7efe4
Aidan Cooney <acooney613@gmail.com>
2023-12-13 16:29:50 -0700

    t

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ fig1.update_layout(
     xaxis_title='Team Payroll',
     yaxis_title='Proportion of Stadium Capacity Filled',
     title='Attendance by Payroll',
-    title_x = 0.5
+    title_x = 0.4
 )
 
 st.plotly_chart(fig1)
